f4413dc2598b421f95ad783d050946f8bfd14a96
luispaulo213 <127565042+luispaulo213@users.noreply.github.com>
2023-11-10 18:55:41 -0300

    Add files via upload

---
+++ catalogo.js
@@ -0,0 +1,6 @@
+let button = document.querySelector(".button")
+let nav = document.querySelector(".nav")
+    button.addEventListener("click",function(){
+    nav.classList.toggle("hidden")
+    }
+)
---
+++ contatos.js
@@ -0,0 +1,8 @@
+
+let button = document.querySelector(".button")
+let nav = document.querySelector(".nav")
+    
+button.addEventListener("click",function(){
+    nav.classList.toggle("hidden")
+    }
+)
